019d0c8cb4f6c5dee2bbccb930ef194167d3967a
Dalton Sutton <dalton@sutton.io>
2024-12-10 20:57:47 -0500

    Updated Currency List

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # Number Flow Display
-A Directus extension that displays a NumberFlow component.
+A Directus extension that displays a Number Flow component.
 
-Based off the incredible work of [NumberFlow](https://github.com/barvian/number-flow) by [barvian](https://github.com/barvian).
+Based off the incredible work of [Number Flow](https://github.com/barvian/number-flow) by [barvian](https://github.com/barvian).
 
 Screenshot:
-![screenshot](https://raw.githubusercontent.com/daltonsutton/directus-extension-numberflow/main/screenshots/numberflow.gif)
+![screenshot](https://raw.githubusercontent.com/daltonsutton/directus-extension-number flow/main/screenshots/numberflow.gif)
 
 ## Installation
 
-Open the Directus Data Studio, navigate to the "Settings" tab, and click on "Marketplace". Search for "NumberFlow" and click on the "Install" button.
+Open the Directus Data Studio, navigate to the "Settings" tab, and click on "Marketplace". Search for "Number Flow" and click on the "Install" button.
 
-Refresh the page, and you should see "NumberFlow" in the Display options when creating a new number field.
+Refresh the page, and you should see "Number Flow" in the Display options when creating a new number field.
 
 ### Alternatively
 
--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import './styles.css';
 
 export default {
 	id: 'numberflow',
-	name: 'NumberFlow',
+	name: 'Number Flow',
 	icon: '123',
-	description: 'NumberFlow component',
+	description: 'Number Flow component',
 	component: DisplayComponent,
 	options: [
 		{
@@ -42,17 +42,177 @@ export default {
 					label: 'Currency Type',
 					choices: [
 						{
-							text: 'USD',
-							value: 'USD',
+							"text": "USD (US Dollar)",
+							"value": "USD"
 						},
 						{
-							text: 'EUR',
-							value: 'EUR',
+							"text": "EUR (Euro)",
+							"value": "EUR"
 						},
 						{
-							text: 'GBP',
-							value: 'GBP',
+							"text": "JPY (Japanese Yen)",
+							"value": "JPY"
 						},
+						{
+							"text": "GBP (British Pound)",
+							"value": "GBP"
+						},
+						{
+							"text": "AUD (Australian Dollar)",
+							"value": "AUD"
+						},
+						{
+							"text": "CAD (Canadian Dollar)",
+							"value": "CAD"
+						},
+						{
+							"text": "CHF (Swiss Franc)",
+							"value": "CHF"
+						},
+						{
+							"text": "CNY (Chinese Yuan)",
+							"value": "CNY"
+						},
+						{
+							"text": "SEK (Swedish Krona)",
+							"value": "SEK"
+						},
+						{
+							"text": "NZD (New Zealand Dollar)",
+							"value": "NZD"
+						},
+						{
+							"text": "KRW (South Korean Won)",
+							"value": "KRW"
+						},
+						{
+							"text": "SGD (Singapore Dollar)",
+							"value": "SGD"
+						},
+						{
+							"text": "NOK (Norwegian Krone)",
+							"value": "NOK"
+						},
+						{
+							"text": "MXN (Mexican Peso)",
+							"value": "MXN"
+						},
+						{
+							"text": "INR (Indian Rupee)",
+							"value": "INR"
+						},
+						{
+							"text": "RUB (Russian Ruble)",
+							"value": "RUB"
+						},
+						{
+							"text": "ZAR (South African Rand)",
+							"value": "ZAR"
+						},
+						{
+							"text": "TRY (Turkish Lira)",
+							"value": "TRY"
+						},
+						{
+							"text": "BRL (Brazilian Real)",
+							"value": "BRL"
+						},
+						{
+							"text": "TWD (New Taiwan Dollar)",
+							"value": "TWD"
+						},
+						{
+							"text": "DKK (Danish Krone)",
+							"value": "DKK"
+						},
+						{
+							"text": "PLN (Polish Zloty)",
+							"value": "PLN"
+						},
+						{
+							"text": "THB (Thai Baht)",
+							"value": "THB"
+						},
+						{
+							"text": "IDR (Indonesian Rupiah)",
+							"value": "IDR"
+						},
+						{
+							"text": "HUF (Hungarian Forint)",
+							"value": "HUF"
+						},
+						{
+							"text": "CZK (Czech Koruna)",
+							"value": "CZK"
+						},
+						{
+							"text": "ILS (Israeli Shekel)",
+							"value": "ILS"
+						},
+						{
+							"text": "CLP (Chilean Peso)",
+							"value": "CLP"
+						},
+						{
+							"text": "PHP (Philippine Peso)",
+							"value": "PHP"
+						},
+						{
+							"text": "AED (United Arab Emirates Dirham)",
+							"value": "AED"
+						},
+						{
+							"text": "COP (Colombian Peso)",
+							"value": "COP"
+						},
+						{
+							"text": "SAR (Saudi Riyal)",
+							"value": "SAR"
+						},
+						{
+							"text": "MYR (Malaysian Ringgit)",
+							"value": "MYR"
+						},
+						{
+							"text": "RON (Romanian Leu)",
+							"value": "RON"
+						},
+						{
+							"text": "VND (Vietnamese Dong)",
+							"value": "VND"
+						},
+						{
+							"text": "EGP (Egyptian Pound)",
+							"value": "EGP"
+						},
+						{
+							"text": "BDT (Bangladeshi Taka)",
+							"value": "BDT"
+						},
+						{
+							"text": "PKR (Pakistani Rupee)",
+							"value": "PKR"
+						},
+						{
+							"text": "NGN (Nigerian Naira)",
+							"value": "NGN"
+						},
+						{
+							"text": "ARS (Argentine Peso)",
+							"value": "ARS"
+						},
+						{
+							"text": "HKD (Hong Kong Dollar)",
+							"value": "HKD"
+						},
+						{
+							"text": "QAR (Qatari Riyal)",
+							"value": "QAR"
+						},
+						{
+							"text": "KWD (Kuwaiti Dinar)",
+							"value": "KWD"
+						}
 					],
 				},
 				width: 'half',
